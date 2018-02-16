28a16a181d7118142bd9fda5810b79903ef70f31
thufir <thufir@dur.bounceme.net>
2018-02-16 01:27:45 -0800

    init

---
+++ src/main/java/net/bounceme/dur/java/soap/weather/App.java
@@ -0,0 +1,16 @@
+package net.bounceme.dur.java.soap.weather;
+
+import java.util.logging.Logger;
+
+public class App {
+
+    private static final Logger log = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        new App().run();
+    }
+
+    private void run() {
+        log.info("begin");
+    }
+}
