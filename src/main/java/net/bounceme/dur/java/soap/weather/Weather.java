476bf6931dd10f1ca83112ef6ccf2c5428a4ad3e
thufir <hawat.thufir@gmail.com>
2018-02-16 01:54:02 -0800

    increment

--- src/main/java/net/bounceme/dur/java/soap/weather/Weather.java
+++ src/main/java/net/bounceme/dur/java/soap/weather/Weather.java
@@ -1,11 +1,8 @@
 package net.bounceme.dur.java.soap.weather;
 
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Logger;
-import javax.xml.stream.Location;
-import javax.xml.ws.WebServiceClient;
 
 public class Weather {
 
@@ -18,33 +15,7 @@ public class Weather {
     }
 
     public void foo() {
-        try (WebServiceClient client = new WebServiceClient.Builder(42, "license_key")
-                .build()) {
-
-            InetAddress ipAddress = InetAddress.getByName("128.101.101.101");
-
-            // Do the lookup
-            CityResponse response = client.city(ipAddress);
-
-            Country country = response.getCountry();
-            System.out.println(country.getIsoCode());            // 'US'
-            System.out.println(country.getName());               // 'United States'
-            System.out.println(country.getNames().get("zh-CN")); // '美国'
-
-            Subdivision subdivision = response.getMostSpecificSubdivision();
-            System.out.println(subdivision.getName());       // 'Minnesota'
-            System.out.println(subdivision.getIsoCode());    // 'MN'
-
-            City city = response.getCity();
-            System.out.println(city.getName());       // 'Minneapolis'
-
-            Postal postal = response.getPostal();
-            System.out.println(postal.getCode());       // '55455'
-
-            Location location = response.getLocation();
-            System.out.println(location.getLatitude());        // 44.9733
-            System.out.println(location.getLongitude());       // -93.2323
-        }
+        log.info("whassup");
     }
 
 }
